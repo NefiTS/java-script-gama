0cfb2a7ee08ef786503212d2a6d6ce3319e3ae63
Nefi <nefisantanna@gmail.com>
2020-05-19 22:48:11 -0300

    Finalizado

--- script.js
+++ script.js
@@ -2,25 +2,27 @@ console.log(" JavaScriptCarregado");
 
 function validaCPF(cpf) { // encapsulando e esperando um texto digitado
     
-    if(cpf.length != 11) { // length infroma quantos caracteres ele tem, sendo 11 digitados nem menos e nem mais
-      return false;
-    } else {
+  if(cpf.length != 11) { // length infroma quantos caracteres ele tem, sendo 11 digitados nem menos e nem mais
+    return false;
+      
+  } else {
 
-      var numeros = cpf.substring(0, 9); // substring a partir de um ponto inicial e final retorna somente o que foi pedido
-      var digitos = cpf.substring(9); // mostra os dois ultimos digitos do cpf
+    var numeros = cpf.substring(0, 9); // substring a partir de um ponto inicial e final retorna somente o que foi pedido
+    var digitos = cpf.substring(9); // mostra os dois ultimos digitos do cpf
 
-      //console.log("numeros do cpf " + numeros);
-      //console.log("digito do cpf " + digitos);
+    //console.log("numeros do cpf " + numeros);
+    //console.log("digito do cpf " + digitos);
 
-      var soma = 0; // var de incremento
+    var soma = 0; // var de incremento
 
-      for(var i = 10; i > 1; i--){ // começando e 10 e indo ate 1
+      for(var i = 10; i > 1; i--) { // começando e 10 e indo ate 1
         soma += numeros.charAt(10 - i) * i; // busca posiçoes ate encontrar e retorna a posição daquela string na lista
-
-
       }
 
       console.log(soma);
+
+      return true;
+      
 
       var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11); // % mod e a sobra da divisão
                        //executando operação, caso positivo executar logo apos o sinal de interregação ate os dois pontos
@@ -34,6 +36,26 @@ function validaCPF(cpf) { // encapsulando e esperando um texto digitado
         return false;
       }
 
+      soma = 0; // resetando a var soma
+
+        numeros = cpf.substring(0, 10); // pegando do primeiro caractere ate o primeiro digito verificador
+
+        for(var k = 11; k > 1; k--){
+
+          soma += numeros.charAt(11 - k) * k;
+
+        }
+
+        //validação do segundo digito
+
+        resultado = soma % 11 > 2 ? 0 : 11 - (soma % 11);
+
+        if (resultado != digitos.charAt(1)){
+
+          return false;
+        }
+        
+
         //console.log(digitos.toString().charAt(0) + " e a primeira posição da var soma"); // toString faz a conveção para string
       
       return true;
@@ -44,6 +66,10 @@ function validaCPF(cpf) { // encapsulando e esperando um texto digitado
 function validacao() {
 
     console.log("Iniciando validação com CPF"); // verificação
+
+    document.getElementById('success').style.display = 'none'; // zerando as mensagens exibidas sendo valido ou invalido
+    document.getElementById('error').style.display = 'none';
+
     var cpf = document.getElementById("cpf_digitado").value;// busca e chama a id verificando o que foi digitado e exibindo, pegando valores da interface
     
 
