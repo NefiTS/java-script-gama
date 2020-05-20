4ee4bcb04cc14075dc0dc1031e9cdc08332e0be9
Nefi <nefisantanna@gmail.com>
2020-05-19 21:16:50 -0300

    Criando proximos passos

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 console.log(" JavaScriptCarregado");
 
-function validaCPF(cpf) {
-    return false;
+function validaCPF(cpf) { // encapsulando e esperando um texto digitado
+    
+    if(cpf.length != 11) { // length infroma quantos caracteres ele tem, sendo 11 digitados nem menos e nem mais
+      return false;
+    } else {
+
+      var numeros = cpf.substring(0, 9); // substring a partir de um ponto inicial e final retorna somente o que foi pedido
+      var digitos = cpf.substring(9); // mostra os dois ultimos digitos do cpf
+
+      //console.log("numeros do cpf " + numeros);
+      //console.log("digito do cpf " + digitos);
+
+      var soma = 0; // var de incremento
+
+      for(var i = 10; i > 1; i--){ // começando e 10 e indo ate 1
+        soma += numeros.charAt(10 - i) * i; // busca posiçoes ate encontrar e retorna a posição daquela string na lista
+
+
+      }
+
+      console.log(soma);
+      
+
+      
+      
+
+      return true;
+    }
+
 }
 
 function validacao() {
 
     console.log("Iniciando validação com CPF"); // verificação
-    var cpf = document.getElementById("cpf_digitado").value;// busca e chama a id verificando o que foi digitado e exibindo
+    var cpf = document.getElementById("cpf_digitado").value;// busca e chama a id verificando o que foi digitado e exibindo, pegando valores da interface
     
 
     var resultadoValidacao = validaCPF(cpf);
